bcb07c916d431f63672af199db4da74fd0bee56a
Nguyenthai27 <ptejthaii@gmail.com>
2022-10-14 14:33:14 +0700

    codejava2

--- src/Ass1/BookLibary.java
+++
@@ -1,4 +0,0 @@
-package Ass1;
-
-public class BookLibary {
-}
---
+++ src/Ass1/HomeController.java
@@ -0,0 +1,15 @@
+package Ass1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class HomeController {
+
+    public void goToBookList(ActionEvent actionEvent) throws Exception {
+        Parent listBook = FXMLLoader.load(getClass().getResource("book/list/list.fxml"));
+        Main.rootStage.setTitle("Books");
+        Main.rootStage.setScene(new Scene(listBook,800,600));
+    }
+}
